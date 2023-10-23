d29ed056b0ddd3858d1b6f91765d9b9c7e63c282
ahmad535353 <mohammadkhani.ahmad53@gmail.com>
2023-10-23 01:01:07 -0600

    Implemented a simple one-time question-answering

--- ConversationManager.py
+++ ConversationManager.py
@@ -1,9 +1,9 @@
 from deepgram import Deepgram
-from API_KEYS import DEEPGRAM_API_KEY
+from API_KEYS import DEEPGRAM_API_KEY, CHATGPT_API_KEY
 import pyaudio
 import logging
 import asyncio
-
+import openai
 
 class ConversationManager():
     def __init__(self) -> None:
@@ -25,7 +25,7 @@ class Transcriber:
     def __init__(self):
         self.api_key = DEEPGRAM_API_KEY
         self.deepgram = Deepgram(self.api_key)
-        self.CHUNK = 20000
+        self.CHUNK = 80000
         self.FORMAT = pyaudio.paInt16
         self.CHANNELS = 1
         self.RATE = 44100
@@ -56,16 +56,29 @@ class Transcriber:
             )
 
             def handle_transcript(t):
-                logging.info(f"Transcript received: {t}")
-                transcripts.append(t)
+                # logging.info(f"Transcript received: {t}")
+                if 'channel' in t:
+                    transcripts.append(t['channel']['alternatives'][0]['transcript'])
+                    # logging.info(f"handled this transcription: {t['channel']['alternatives'][0]['transcript']}")
+
 
             deepgramLive.registerHandler(deepgramLive.event.TRANSCRIPT_RECEIVED, handle_transcript)
 
+            silent_time = 0  # Time duration for which silence has been detected
             while True:
                 data = stream.read(self.CHUNK)
                 if not data:
                     logging.warning("No data read from the microphone.")
                 else:
+                    # Check for silence
+                    if self.is_silent(data):
+                        silent_time += (self.CHUNK / self.RATE)
+                        if silent_time > 5:  # 5 seconds of silence
+                            logging.info("Silence detected. Stopping listening.")
+                            break
+                    else:
+                        silent_time = 0  # Reset the silence timer
+
                     logging.debug("Captured audio data from microphone.")
                     deepgramLive.send(data)
                 logging.debug("Sent audio data to Deepgram.")
@@ -78,18 +91,32 @@ class Transcriber:
             self.audio.terminate()
 
         await deepgramLive.finish()
-        print (transcripts)
-        return transcripts
+        print (transcripts[:-1])
+        return transcripts[:-1]
+    
+    def is_silent(self, data):
+        SILENCE_THRESHOLD = 500
+        """Returns 'True' if below the 'silent' threshold"""
+        return max(data) < SILENCE_THRESHOLD
 
 class LLM:
     def __init__(self):
         # Initialize LLM SDK or API connection here if needed
-        pass
+        openai.api_key = CHATGPT_API_KEY
 
     def process(self, text):
         # Process the text with LLM and return the response
-        response = "AI Response for " + text  # Mock response
-        return response
+        completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a conversational assistant. You should asnwer every question like a real human, like a conversation. Keep the answers short."},
+            {"role": "user", "content": text}
+        ]
+        )
+        answer = completion.choices[0].message
+        print (answer)
+        # response = "AI Response:" + answer  # Mock response
+        return answer
 
 class Synthesizer:
     def __init__(self):
